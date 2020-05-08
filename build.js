425575a82c2e920a036f2c04276e8ebf58175932
Matt Luongo <mhluongo@gmail.com>
2020-05-08 00:30:53 -0400

    Pull Gnosis Safe contracts into the build

--- build.js
+++ build.js
@@ -3,40 +3,53 @@ const path = require('path')
 const colors = require('colors')
 const glob = require('glob-promise')
 
+function copyArtifacts(targetDir, destDir) {
+  return glob(path.join(targetDir, '/*.json')).then((files) => {
+    let dirResult = fsPromises.mkdir(destDir).then(
+      v => v,
+      err => {
+        if (err.code && err.code === 'EEXIST') {
+          console.warn('Artifacts directory exists...')
+        } else {
+          throw err
+        }
+      }
+    )
+
+    let results = files.map((file) => {
+      let base = path.basename(file)
+      let newFile = path.join(destDir, path.basename(file))
+      return fsPromises.copyFile(file, newFile).then(
+        () => console.log('Copied artifact: ', base.green),
+        (err) => console.error(`Couldn't copy artifact: ${base.red}`)
+      )
+    })
+
+    return Promise.all(results)
+  })
+}
+
 let keepArtifactsDir = path.dirname(
   require.resolve("@keep-network/keep-core/artifacts/KeepToken.json"))
 
+let gnosisArtifactsDir = path.dirname(
+  require.resolve("@gnosis.pm/safe-contracts/build/contracts/GnosisSafe.json"))
+
 let localArtifacts = path.join(__dirname, '/artifacts/')
 
-glob(path.join(keepArtifactsDir, '/*.json')).then((files) => {
-
-  let dirResult = fsPromises.mkdir(localArtifacts).then(
-    v => v,
-    err => {
-      if (err.code && err.code === 'EEXIST') {
-        console.warn('Artifacts directory exists...')
-      } else {
-        throw err
-      }
-    }
-  )
-
-  let results = files.map((file) => {
-    let base = path.basename(file)
-    let newFile = path.join(localArtifacts, path.basename(file))
-    return fsPromises.copyFile(file, newFile).then(
-      () => console.log('Copied artifact: ', base.green),
-      (err) => console.error(`Couldn't copy artifact: ${base.red}`)
-    )
-  })
-
-  return Promise.all([dirResult].concat(results)).then(
-    (values) => console.log('Successfully copied Keep contract artifacts!'.green)
-  )
-}).then(
+Promise.all([
+  copyArtifacts(
+    keepArtifactsDir,
+    path.join(localArtifacts, '/keep-network/')
+  ).then((v) => console.log('Successfully copied Keep contract artifacts!')),
+  copyArtifacts(
+    gnosisArtifactsDir,
+    path.join(localArtifacts, '/gnosis/')
+  ).then((v) => console.log('Successfully copied Gnosis contract artifacts!'))
+]).then(
   (v) => v,
   (err) => console.error(
-    'Error copying Keep contract artifacts!',
+    'Error copying contract artifacts!',
     err.toString().red
   )
 )
