2a2629514d53b2963802688ec3477185b6af282d
Matt Luongo <mhluongo@gmail.com>
2020-05-31 23:27:27 -0400

    Support staking via grant in the CLI

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import inquirer from 'inquirer'
 
 import { ethers } from 'ethers'
 import { Zero, AddressZero } from 'ethers/constants'
-import { UnsignedTransaction } from 'ethers/utils'
+import { serializeTransaction, UnsignedTransaction } from 'ethers/utils'
 import * as ledger from './ledger'
 
 import { removeHexPrefix } from './utils'
@@ -17,6 +17,21 @@ import * as keep from './keep'
 
 function abbreviatedAddress(address : string) : string {
   return `${address.substr(0, 5)}...${address.slice(-3)}`
+}
+
+function ethereumAddressQuestion(name : string, message : string) {
+  return {
+    name,
+    message,
+    validate: (value: string) => {
+      try {
+        ethers.utils.getAddress(value)
+        return true
+      } catch (e) {
+        return 'Enter a valid Ethereum address'
+      }
+    }
+  }
 }
 
 function formattedSafeDetails(safe : gnosis.Safe, details : gnosis.SafeDetails,
@@ -53,29 +68,23 @@ async function getSafeFromUser(provider: ethers.providers.Provider)
   let safeDetails
 
   while (true) {
-    let answers = await inquirer.prompt([{
-      name: 'useSafe',
-      message: 'Use a Gnosis Safe?',
-      type: 'confirm'
-    }, {
-      name: 'safeAddress',
-      message: 'Gnosis Safe address:',
-      validate: (value: string) => {
-        try {
-          ethers.utils.getAddress(value)
-          return true
-        } catch (e) {
-          return 'Enter a valid Ethereum address'
-        }
+    let answers = await inquirer.prompt([
+      {
+        name: 'useSafe',
+        message: 'Use a Gnosis Safe?',
+        type: 'confirm'
       },
-      when: (answers) => answers.useSafe,
-    }])
+      {
+        ...ethereumAddressQuestion('safeAddress', 'Gnosis Safe address:'),
+        when: (answers) => answers.useSafe,
+      }
+    ])
 
     if (!answers.useSafe) {
       return null
     }
 
-    safe = await gnosis.Safe.fromSolidity(answers.safeAddress, provider)
+    safe = await gnosis.Safe.fromSolidity(answers.safeAddress as string, provider)
 
     let newAnswers = (await inquirer.prompt([{
       name: 'confirmed',
@@ -115,31 +124,26 @@ async function getGrantFromUser(provider: ethers.providers.Provider)
   let safeDetails
 
   while (true) {
-    let answers = await inquirer.prompt([{
-      name: 'useManagedGrant',
-      message: 'Use a managed grant?',
-      type: 'confirm'
-    }, {
-      name: 'grantAddress',
-      message: 'Managed grant address:',
-      validate: (value: string) => {
-        try {
-          ethers.utils.getAddress(value)
-          return true
-        } catch (e) {
-          return 'Enter a valid Ethereum address'
-        }
+    let answers = await inquirer.prompt([
+      {
+        name: 'useManagedGrant',
+        message: 'Use a managed grant?',
+        type: 'confirm'
       },
-      when: (answers) => answers.useManagedGrant,
-    },{
-      name: 'grantId',
-      type: 'number',
-      message: 'Grant ID:',
-      validate: (value: string) => !isNaN(parseInt(value)),
-      when: (answers) => !answers.useManagedGrant,
-    }])
+      {
+        ...ethereumAddressQuestion('grantAddress', 'Managed grant address:'),
+        when: (answers) => answers.useManagedGrant,
+      },
+      {
+        name: 'grantId',
+        type: 'number',
+        message: 'Grant ID:',
+        validate: (value: string) => !isNaN(parseInt(value)),
+        when: (answers) => !answers.useManagedGrant,
+      }
+    ])
 
-    grant = await keep.ManagedGrant.fromSolidity(answers.grantAddress, provider)
+    grant = await keep.ManagedGrant.fromSolidity(answers.grantAddress as string, provider)
 
     let newAnswers = (await inquirer.prompt([{
       name: 'confirmed',
@@ -165,12 +169,19 @@ async function signMessageWithLedger(message : string) : Promise<string> {
 
   const ethApp = await ledger.initializeEthApp()
 
-  const dataSig = await ledger.signPersonalMessage(ethApp, message)
+  const sig = await ledger.signPersonalMessage(ethApp, message)
 
-  return ethers.utils.joinSignature(dataSig).replace(/1b$/, '1f').replace(/1c$/, '20')
+  return ethers.utils.joinSignature(sig).replace(/1b$/, '1f').replace(/1c$/, '20')
 }
 
-async function signMessage(message : string, address? : string) {// : Promise<string> {
+async function signTxWithLedger(tx : UnsignedTransaction) : Promise<string> {
+
+  const ethApp = await ledger.initializeEthApp()
+
+  return await ledger.signTransaction(ethApp, tx)
+}
+
+async function signMessage(message : string, address? : string) : Promise<string> {
   console.log(`Message to be signed: ${chalk.cyan(message)}`)
 
   let signature// : Signature
@@ -212,8 +223,39 @@ async function signMessage(message : string, address? : string) {// : Promise<st
   }
 }
 
+async function signTx(tx : UnsignedTransaction, address? : string) : Promise<string> {
+  console.log(`Tx to be signed: ${chalk.cyan(JSON.stringify(tx, null, 2))}`)
+  console.log(`Serialized tx: ${chalk.cyan(serializeTransaction(tx))}`)
+
+  let signature
+
+  let answers = await inquirer.prompt([
+    {
+      name: 'useLedger',
+      type: 'confirm',
+      message: 'Would you like to sign with an attached Ledger?'
+    },
+    {
+      name: 'manuallySignedTx',
+      when: (answers) => !answers.useLedger,
+      message: (answers) => `Signature:`,
+    }
+  ])
+
+  if (answers.useLedger) {
+    signature = await signTxWithLedger(tx)
+  } else {
+    signature = answers.manuallySignedTx
+  }
+
+  return signature
+}
+
 async function prepareSafeTransaction(tx : UnsignedTransaction,
   safe : gnosis.Safe) : Promise<UnsignedTransaction> {
+
+  console.log('Preparing safe transaction...\n')
+
   let details = await safe.getDetails()
 
   const txHash = await safe.getTransactionHash(tx.to || '', Zero, tx.data || '',
@@ -246,15 +288,7 @@ async function prepareSafeTransaction(tx : UnsignedTransaction,
     tx.to || '', Zero, tx.data || '', Zero, Zero, signatures)
 }
 
-async function withdrawFromGrant(grant : keep.ManagedGrant, safe : gnosis.Safe) {
-  console.log(`Preparing tx to withdraw from grant ${chalk.cyan(grant.address)}...`)
-  let tx = grant.getWithdrawalTx()
-
-  console.log(`${chalk.bold('Withdrawal TX:')}: ${JSON.stringify(tx, null, 2)}\n`)
-
-  let safeTx = await prepareSafeTransaction(tx, safe)
-
-  console.log(`${chalk.bold('Safe TX')}: ${JSON.stringify(safeTx, null, 2)}\n`)
+async function signAndBroadcast(tx : UnsignedTransaction) : Promise<string | null> {
 
   let answers = await inquirer.prompt([
     {
@@ -272,6 +306,87 @@ async function withdrawFromGrant(grant : keep.ManagedGrant, safe : gnosis.Safe) 
 
   if (answers.sign) {
   }
+  return null
+}
+
+async function withdrawFromGrant(safe : gnosis.Safe, provider : ethers.providers.Provider) {
+
+  let grant = await getGrantFromUser(provider)
+
+  console.log(`Preparing tx to withdraw from grant ${chalk.cyan(grant.address)}...`)
+  let tx = grant.getWithdrawalTx()
+
+  console.log(`${chalk.bold('Withdrawal TX:')}: ${JSON.stringify(tx, null, 2)}\n`)
+
+  let safeTx = await prepareSafeTransaction(tx, safe)
+
+  await signAndBroadcast(safeTx)
+}
+
+async function stakeFromGrant(safe : gnosis.Safe, provider : ethers.providers.Provider) {
+  let grant = await getGrantFromUser(provider)
+
+  let answers : {
+    stakingContract : string,
+    amount : number,
+    operator : string,
+    authorizer : string,
+    beneficiary : string,
+    confirmDetails: boolean,
+  }
+
+  while (true) {
+    answers = await inquirer.prompt([
+      ethereumAddressQuestion('stakingContract', 'Staking contract address:'),
+      {
+        name: 'amount',
+        message: 'Amount of KEEP (min 100k):',
+        type: 'number',
+        validate: (n : number) => (n >= 100000 ?
+          true : 'Must stake at >=100k KEEP'),
+      },
+      ethereumAddressQuestion(
+        'operator', 'Operator address. This is the staking node:'),
+      ethereumAddressQuestion('authorizer', 'Authorizer address. ' +
+        'If you\'re unsure, use the owner address or another you control:'),
+      ethereumAddressQuestion(
+        'beneficiary', 'Beneficiary address - where rewards will be sent:'),
+    ])
+
+    let table = new Table()
+    table.push(
+      { stakingContract: answers.stakingContract },
+      { amount: `${answers.amount} KEEP` },
+      { operator: answers.operator },
+      { authorizer: answers.authorizer },
+      { beneficiary: answers.beneficiary },
+    )
+
+    let newAnswers = await inquirer.prompt([
+      {
+        name: 'confirmDetails',
+        type: 'confirm',
+        message: `${table.toString()} \n\nDoes this look right? (Y/n):`
+      }
+    ])
+
+    if (answers.confirmDetails) {
+      break
+    }
+  }
+
+  console.log(`Preparing tx to stake from grant ${chalk.cyan(grant.address)}...`)
+  const tx = grant.getStakingTx(answers.stakingContract,
+    keep.keepToWei(answers.amount), answers.operator, answers.authorizer,
+    answers.beneficiary)
+
+  console.log(`${chalk.bold('Staking TX:')}: ${JSON.stringify(tx, null, 2)}\n`)
+
+  const safeTx = await prepareSafeTransaction(tx, safe)
+
+  console.log(`${chalk.bold('Safe TX')}:\n`)
+
+  await signAndBroadcast(safeTx)
 }
 
 const main = async () => {
@@ -334,10 +449,14 @@ const main = async () => {
     }])
 
     switch (answers.choice) {
+      case 'stake-grant':
+        if (safe !== null) {
+          await stakeFromGrant(safe, provider)
+        }
+        break
       case 'withdraw-grant':
         if (safe !== null) {
-          let grant = await getGrantFromUser(provider)
-          await withdrawFromGrant(grant, safe)
+          await withdrawFromGrant(safe, provider)
         }
         break
     }
